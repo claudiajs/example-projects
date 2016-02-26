a27ead6ccb6c8f43809873bcbd4ca16dd00f7e6e
Gojko Adzic <gojko@gojko.com>
2016-02-26 14:54:31 +0100

    example for path params

--- web-api/web.js
+++ web-api/web.js
@@ -4,19 +4,34 @@ var ApiBuilder = require('claudia-api-builder'),
 	Promise = require('bluebird'),
 	superb = require('superb');
 module.exports = api;
+
+// just return the result value for synchronous processing
 api.get('/hello', function () {
 	'use strict';
 	return 'hello world';
 });
+
+// pass some arguments using the query string or headers to this
+// method and see that they're all in the request object
 api.get('/echo', function (request) {
 	'use strict';
 	return request;
 });
-api.post('/echo', function (request) {
-	'use strict';
-	return request;
-});
+
+// use a promise for asynchronous processing
 api.get('/greet', function (request) {
 	'use strict';
 	return Promise.resolve(request.queryString.name + ' is ' + superb());
+});
+
+// use {} for dynamic path parameters
+api.get('/people/{name}', function (request) {
+	'use strict';
+	return 'You wanted ' + request.pathParams.name;
+});
+
+// use .post to handle a post; or .delete, .patch, .head, .put
+api.post('/echo', function (request) {
+	'use strict';
+	return request;
 });
