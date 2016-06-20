e91293dd4246773b119222263f49f4ac8f477db1
Gojko Adzic <gojko@gojko.com>
2016-06-20 06:36:54 +0200

    using fb template builder

--- bot-with-buttons/bot.js
+++ bot-with-buttons/bot.js
@@ -1,11 +1,11 @@
 /*global module*/
 'use strict';
 const builder = require('claudia-bot-builder'),
-  fbTemplate = require('claudia-bot-builder').fbTemplate,
+  fbTemplate = builder.fbTemplate,
   ws = require('./src/ws'),
-  eTitle = entity => ((entity.label || '') + ' ' + (entity.description || ''));
+  eTitle = entity => ((entity.label || '') + ' ' + (entity.description || '')),
+  format = text => (text && text.substring(0, 80));
 module.exports = builder(request => {
-  console.log('received', request);
   return ws.findEntities(request.text).then(r => r.results).then(entities => {
     if (!entities.length) {
       return `Unfortunately, could not find anything about ${request.text} in Wikidata`;
@@ -14,9 +14,8 @@ module.exports = builder(request => {
       return ws.entityClaims(entities[0].id).then(claims => title + ':\n' + claims.join('\n'));
     } else {
       const generic = new fbTemplate.generic();
-      entities.slice(0, 9).forEach(entity => ({
-        generic
-          .addBubble(entity.label.substring(0, 80), entity.description.substring(0, 80))
+      entities.slice(0, 9).forEach(entity => {
+        generic.addBubble(format(entity.label), format(entity.description))
             .addButton('View Facts', entity.id);
       });
       return generic.get();
