7517968f8790693c16256b66b81b25198f63a66a
Giacomo Fabbian <giacomo.fabbian@studenti.unipd.it>
2020-01-02 10:25:04 +0100

    Update app.js

--- fastify-app-lambda/app.js
+++ fastify-app-lambda/app.js
@@ -1,15 +1,18 @@
 const fastify = require('fastify');
 
-const app = fastify();
-app.get('/', (request, reply) => reply.send({ hello: 'world' }));
+function init() {
+  const app = fastify();
+  app.get('/', (request, reply) => reply.send({ hello: 'world' }));
+  return app;
+}
 
-if (require.main !== module) {
+if (require.main === module) {
   // called directly i.e. "node app"
-  app.listen(3000, (err) => {
+  init().listen(3000, (err) => {
     if (err) console.error(err);
     console.log('server listening on 3000');
   });
 } else {
   // required as a module => executed on aws lambda
-  module.exports = app;
+  module.exports = init;
 }
